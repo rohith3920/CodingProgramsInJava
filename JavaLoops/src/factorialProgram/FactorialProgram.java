b854d35b27e2f985d9366522bff8d931b25c1cb3
rohith <rohithreddy@3920>
2024-04-04 20:59:32 +0530

    started working on the java loops

---
+++ JavaLoops/src/factorialProgram/FactorialProgram.java
@@ -0,0 +1,20 @@
+package factorialProgram;
+import java.util.*;
+
+public class FactorialProgram {
+
+	public static void main(String[] args) {
+		int n, fact = 1;
+		Scanner sc = new Scanner(System.in) ;
+		System.out.println("enter the number");
+		
+		n = sc.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			fact = fact * i;
+		}
+		System.out.println("fact " + fact);
+
+	}
+
+}
